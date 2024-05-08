1ae342ce05e16f09e0ffdf7709de9f2b97c594f4
aya0620a <aotomiyabi2525@gmail.com>
2024-05-08 16:01:06 +0900

    修正

--- roman.rb
+++ roman.rb
@@ -1,38 +1,47 @@
-def roman(arabic)
-  roman = ''
-  if arabic == 4
-    arabic.times do
-      roman += 'I'
-    end
-    roman = 'IV'
-  elsif arabic == 5
-    roman = 'V'
-  elsif arabic == 9
-    arabic.times do
-      roman += 'I'
-    end
-    roman = 'IX'
-  elsif arabic >= 10
-    roman = 'X'
-    roman += roman(arabic-10)
-  else
-    arabic.times do
-      roman += 'I'
-    end
-  end
-end
+# def roman(arabic)
+#   roman = ''
+#   if arabic == 1
+
+#   if arabic == 4
+#     arabic.times do
+#       roman += 'IV'
+#     end
+#     roman = 'IV'
+#   elsif arabic == 5
+#     roman = 'V'
+#   elsif arabic.times do
+#     roman += 'I'
+#   end
+#   return roman
+# end
 
 def to_roman(arabic)
   roman = ''
-  arabic.times do
-    roman += 'I'
-  end
-  retunr roman
+  p quo= arabic/10
+  roman += 'X'*quo
+  p resideual = arabic%10
+  roman += 'I'*resideual
+  return roman
 end
 
-puts "index expected actual"
-[[1,'I'],[2, 'II'], [3, 'III'], [4, 'IV'], [5, 'VI'], []
+def re_to_roman(arabic)
+  roman = ''
+  p quo= arabic/10
+  roman += 'X'*quo
+  p resideual = arabic%10
+  roman += 'I'*resideual
+  return roman
+end
+
+# puts "index expected actual"
+
+[
+  [1,'I'],
+  # [2, 'II'],
+  # [3, 'III'],
+  [6, "VI"],
+  [11, 'xI'],
 ].each do |arabic, expected|
-  actual = roman(arabic)
+  actual = to_roman(arabic)
   p [arabic, expected, actual]
 end
