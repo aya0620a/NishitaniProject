49d527b499deb39039058c8a282c276ace20557c
aya0620a <aotomiyabi2525@gmail.com>
2024-06-05 16:33:55 +0900

    day8

---
+++ Hello.md
@@ -0,0 +1,9 @@
+# Hello
+```ruby
+p name = ARGV[0] ||"Aoto Nishi"
+puts "Hello #{name}!"
+p num = ARGV[1].to_f   #型変換
+puts "Hello #{num}!"
+
+```
+
--- fsm.rb
+++ fsm.rb
@@ -1,13 +1,33 @@
 require 'pp'
-file ='tmp.md'
-#　＃と```をキーとする
-keys = ['#', '```']
-state = 'head_search'
-File.readlines(file)[0..4].each do |line, i| #readlineは一行ずつ読み込む readは全部読み込む
-  p [ state, line, line.include?(keys[0])]
-    if line.include?(keys[0])
-      p 'block_head'
-      state = 'block_search'
-    end
-  # lineにキーが含まれているか
+def write_file(cont)
+  p file_name  = cont[0][2..-3]+".md"
+  File.write(file_name, cont.join(''))
 end
+
+file = 'tmp.md'
+keys =['# ','```']
+state= :head_search
+cont = []
+File.readlines(file)[0..11].each do |line|
+    case state
+    when :head_search
+        # p [state,line.include?(keys[0]),line]
+        if line.include?(keys[0])
+            if cont[0]
+                write_file(cont)
+            end
+            cont = [line]
+            state = :block_search
+        else
+            cont << line
+        end
+    when :block_search
+        # p [state,line.include?(keys[1]),line]
+        if line.include?(keys[1])
+            cont << line
+            state = :head_search
+        end
+    end
+
+end
+write_file(cont)
